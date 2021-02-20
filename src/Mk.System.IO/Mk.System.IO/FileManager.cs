57643b9c5978bc9428ecb9c7e576c0652cd085c2
Mike Glombitza <mike-glombitza@web.de>
2021-02-20 16:26:50 +0100

    Add File Saving, Moving and Json serialization and deserialization

---
+++ src/Mk.System.IO/Mk.System.IO/FileManager.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Mk.System.IO
+{
+    public static class FileManager
+    {
+        public static void SaveFile(string content, string path, string fileName)
+        {
+            File.WriteAllText(path + fileName, content);
+        }
+
+        public static string ReadFile(string filePath)
+        {
+            if (File.Exists(filePath))
+                return File.ReadAllText(filePath);
+            return string.Empty;
+        }
+    }
+}
---
+++ src/Mk.System.IO/Mk.System.IO/JSON/JSONConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Mk.System.IO.JSON
+{
+    public static class JSONConverter
+    {
+        public static string SerializeObject<T>(T value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        public static T DeserializeObject<T>(string filePath)
+        {
+            
+            if (File.Exists(filePath))
+            {
+                var json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            return default;
+        }
+    }
+}
